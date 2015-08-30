aaa5b8e1ef8b8bef976a7936a46df63336112623
Mike Flynn <mfflynn@gmail.com>
2015-08-30 00:32:21 -0700

    First commit.

---
+++ Humanize Her/ViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  Humanize Her
+//
+//  Created by Mike Flynn on 8/28/15.
+//  Copyright (c) 2015 That Mike Flynn. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    let captureSession = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    var captureDevice : AVCaptureDevice?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        
+        let devices = AVCaptureDevice.devices()
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
+        
+        if captureDevice != nil {
+            beginVideoSession()
+        }
+    }
+    
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var overlayView: UIView!
+    
+    @IBAction func filterDoctor(sender: AnyObject) {
+        updateFilter("doctor.png")
+    }
+    
+    @IBAction func filterNone(sender: AnyObject) {
+        updateFilter("transparent.png")
+    }
+    
+    @IBAction func filterSuit(sender: AnyObject) {
+        updateFilter("suit.png")
+    }
+    
+    func updateFilter(assetName: String) {
+        let bounds:CGRect = overlayView.bounds
+        
+        for view in overlayView.subviews{
+            view.removeFromSuperview()
+        }
+        
+        let image = UIImage(named: assetName)
+        let imageView = UIImageView(image: image!)
+        imageView.contentMode = .ScaleAspectFill
+        imageView.frame = CGRect(x: 0, y: 0, width: CGRectGetWidth(bounds), height: CGRectGetHeight(bounds))
+        overlayView.addSubview(imageView)
+    }
+    
+    func configureDevice() {
+        if let device = captureDevice {
+            try! device.lockForConfiguration()
+            device.focusMode = .AutoFocus
+            device.unlockForConfiguration()
+        }
+    }
+    
+    func beginVideoSession() {
+        configureDevice()
+        
+        var err : NSError? = nil
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice) as AVCaptureDeviceInput
+            captureSession.addInput(input)
+        } catch let error as NSError {
+            err = error
+        }
+        
+        if err != nil {
+            print("error: \(err?.localizedDescription)")
+        }
+        
+        let bounds:CGRect = self.view.bounds
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect; // AVLayerVideoGravityResizeAspectFill
+        //previewLayer?.bounds = bounds
+        //previewLayer?.frame = self.view.frame
+        previewLayer?.frame = CGRect(x: 0, y: -25, width: CGRectGetWidth(bounds), height: CGRectGetHeight(bounds))
+        videoView.layer.addSublayer(previewLayer!)
+        captureSession.startRunning()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Humanize HerTests/Humanize_HerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Humanize_HerTests.swift
+//  Humanize HerTests
+//
+//  Created by Mike Flynn on 8/28/15.
+//  Copyright (c) 2015 That Mike Flynn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Humanize_HerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
